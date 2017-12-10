0b21daf55a61604cb525276b53c1dd0ddaa062e2
DullBird14 <524318257@qq.com>
2017-12-10 17:45:22 +0800

    市场买卖测试通过

--- src/test/java/com/cys/chapter/four/fourChapterTest.java
+++ src/test/java/com/cys/chapter/four/fourChapterTest.java
@@ -1,10 +1,12 @@
 package com.cys.chapter.four;
 
 import com.cys.chapter.util.JedisClientFactory;
-import redis.clients.jedis.Jedis;
-import redis.clients.jedis.Pipeline;
-import redis.clients.jedis.Response;
-import redis.clients.jedis.Transaction;
+import redis.clients.jedis.*;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.Set;
 
 /**
  * 商品市场，实现事务交易
@@ -12,6 +14,9 @@ import redis.clients.jedis.Transaction;
 public class fourChapterTest {
 	public Jedis client = JedisClientFactory.getInstance();
 
+	fourChapterTest(){
+		init();
+	}
 	/**
 	 * 初始化商品市场
 	 */
@@ -20,12 +25,8 @@ public class fourChapterTest {
 		client.hset("users:17", "funds", "43");
 		client.hset("users:27", "name", "xiaoHong");
 		client.hset("users:27", "funds", "125");
-		client.set("goods:17", "itemL");
-		client.set("goods:17", "itemM");
-		client.set("goods:17", "itemN");
-		client.set("goods:27", "itemO");
-		client.set("goods:27", "itemP");
-		client.set("goods:27", "itemQ");
+		client.sadd("goods:17", "itemL", "itemM", "itemN");
+		client.sadd("goods:27", "itemO", "itemP", "itemQ");
 	}
 
 	/**
@@ -43,16 +44,16 @@ public class fourChapterTest {
 
 		while (time > System.currentTimeMillis()) {
 			try {
-				pipelined.watch(goodKey);
-				if (!pipelined.sismember(goodKey, sellItemName).get()) {
+				client.watch(goodKey);
+				if (!client.sismember(goodKey, itemId)) {
 					client.unwatch();
 					return false;
 				}
-
 				pipelined.multi();
 				pipelined.zadd("market:", price, sellItemName);
 				pipelined.srem(goodKey, itemId);
 				pipelined.exec();
+				pipelined.close();
 				return true;
 			} catch (Exception e) {
 				e.printStackTrace();
@@ -78,27 +79,116 @@ public class fourChapterTest {
 
 		while (time > System.currentTimeMillis()) {
 			try {
-				pipelined.watch("market", buyerKey);
-				Double price = pipelined.zscore("maket", itemMarketName).get();
-				Double buyerFounds = Double.parseDouble(pipelined.hget(buyerKey, "founds").get());
+				pipelined.watch("market:", buyerKey);
+				Response<Double> maket = pipelined.zscore("market:", itemMarketName);
+				Response<String> founds = pipelined.hget(buyerKey, "funds");
+				pipelined.sync();
+				Double price = maket.get();
+				Double buyerFunds = Double.parseDouble(founds.get());
 				if ((!lprice.equals(price))
-                        || buyerFounds.doubleValue() < lprice.doubleValue()) {
+                        || buyerFunds.doubleValue() < lprice.doubleValue()) {
                     client.unwatch();
                     //信息已经变化或者钱不够
                     return false;
                 }
 
 				pipelined.multi();
-				pipelined.hincrBy(sellerKey, "founds", lprice.longValue());
-				pipelined.hincrBy(buyerKey, "founds", -lprice.longValue());
+				pipelined.hincrBy(sellerKey, "funds", lprice.longValue());
+				pipelined.hincrBy(buyerKey, "funds", -lprice.longValue());
 				pipelined.zrem("market:", itemMarketName);
 				pipelined.sadd("goods:" + buyerId, itemId);
 				pipelined.exec();
+				pipelined.close();
 				return true;
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
 		return false;
+	}
+
+	/**
+	 * 一个测试打印结果方法
+	 */
+	void  printlnMessage(){
+		synchronized (fourChapterTest.class) {
+			System.out.println(Thread.currentThread());
+			Map<String, String> userInfoOne = client.hgetAll("users:27");
+			System.out.println("=============users:27====================");
+			for (Map.Entry<String, String> entry : userInfoOne.entrySet()) {
+                System.out.println(entry.getKey() + "  :  " + entry.getValue());
+            }
+			System.out.println("=============goods:27====================");
+			System.out.println(client.smembers("goods:27"));
+			Map<String, String> userInfoTwo = client.hgetAll("users:17");
+			System.out.println("=============users:17====================");
+			for (Map.Entry<String, String> entry : userInfoTwo.entrySet()) {
+                System.out.println(entry.getKey() + "  :  " + entry.getValue());
+            }
+			System.out.println("=============goods:17====================");
+			System.out.println(client.smembers("goods:17"));
+			Set<Tuple> market = client.zrangeWithScores("market:", 0, -1);
+			System.out.println("=============market====================");
+			for (Tuple tuple : market) {
+                System.out.println(tuple.getElement() + "::" + tuple.getScore());
+            }
+		}
+	}
+
+	public static void main(String[] args) {
+		final fourChapterTest test = new fourChapterTest();
+		final fourChapterTest testTwo = new fourChapterTest();
+
+//		test.putItemToMarket("itemL", "17", 66.0);
+//		test.printlnMessage();
+//		test.purchaseGood("27","itemL", "17", 66.0);
+//		test.printlnMessage();
+		Thread one = new Thread(new Runnable() {
+			public void run() {
+				while (true) {
+					if (test.putItemToMarket("itemL", "17", 66.0)) {
+						System.out.println("17上架成功");
+					}
+					test.printlnMessage();
+//					Random rand =new Random(500);
+//					try {
+//						System.out.println("17进入sleep");
+//						Thread.currentThread().sleep(rand.nextInt());
+//					} catch (InterruptedException e) {
+//						e.printStackTrace();
+//					}
+//					System.out.println("17结束sleep");
+					if (test.purchaseGood("17", "itemL", "27", 66.0)) {
+						System.out.println("17购买成功");
+					}
+					test.printlnMessage();
+				}
+			}
+		}, "Thread-one");
+
+		Thread two = new Thread(new Runnable() {
+			public void run() {
+				while (true) {
+					if (testTwo.putItemToMarket("itemL", "27", 66.0)) {
+						System.out.println("27上架成功");
+					}
+					testTwo.printlnMessage();
+//					Random rand =new Random(500);
+//					try {
+//						System.out.println("27进入sleep");
+//						Thread.currentThread().sleep(rand.nextInt());
+//					} catch (InterruptedException e) {
+//						e.printStackTrace();
+//					}
+//					System.out.println("27醒来");
+					if (testTwo.purchaseGood("27", "itemL", "17", 66.0)) {
+						System.out.println("27购买成功");
+					}
+					testTwo.printlnMessage();
+				}
+			}
+		}, "Thread-two");
+		one.start();
+		two.start();
 	}
 }
